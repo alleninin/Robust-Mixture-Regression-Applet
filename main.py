796b5684da87cf5d57bf03cc250bd280469d2e0f
alleninin <87659430+alleninin@users.noreply.github.com>
2026-04-07 21:14:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Main Regression Hub")
+
+st.title("Main Regression Analysis")
+st.write("welcome! use the sidebar to switch between different analysis modes.")
+st.write("\n in 'custom synthetic data generation,' choose the different parameters for generating synthetic data, and tune the parameters of the mixture regression model accordingly.")
